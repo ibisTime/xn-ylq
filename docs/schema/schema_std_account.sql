a069804d8b0433b89ac2b287c7d137758361556b
haiqingzheng <leo.zheng@hichengdai.com>
2017-09-20 21:11:42 +0800

    v120脚本整理

--- docs/schema/schema_std_account.sql
+++ docs/schema/schema_std_account.sql
@@ -276,6 +276,27 @@ CREATE TABLE `tstd_exchange_currency` (
   PRIMARY KEY (`code`) COMMENT '币种兑换'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `tbf_pay_order` (
+  `code` varchar(32) NOT NULL COMMENT '宝付订单号',
+  `trans_orderid` varchar(64) DEFAULT NULL COMMENT '宝付订单号',
+  `trans_batchid` varchar(64) DEFAULT NULL COMMENT '宝付批次号',
+  `trans_no` varchar(32) DEFAULT NULL COMMENT '商户订单号',
+  `trans_money` bigint(32) DEFAULT NULL COMMENT '转账金额',
+  `to_acc_name` varchar(64) DEFAULT NULL COMMENT '收款人姓名',
+  `to_acc_no` varchar(64) DEFAULT NULL COMMENT '收款人银行帐号',
+  `to_acc_dept` varchar(128) DEFAULT NULL COMMENT '收款人开户行机构名',
+  `trans_summary` varchar(255) DEFAULT NULL COMMENT '摘要',
+  `trans_fee` bigint(32) DEFAULT NULL COMMENT '交易手续费',
+  `state` varchar(32) DEFAULT NULL COMMENT '状态（0-转账中 1-转账成功 -1-转账失败 2-转账退款）',
+  `trans_remark` varchar(255) DEFAULT NULL COMMENT '备注（错误信息）',
+  `trans_starttime` datetime DEFAULT NULL COMMENT '交易申请时间',
+  `trans_endtime` datetime DEFAULT NULL COMMENT '交易完成时间',
+  `back_url` varchar(255) DEFAULT NULL COMMENT '回调地址',
+  `company_code` varchar(32) DEFAULT NULL COMMENT '公司编号',
+  `system_code` varchar(32) DEFAULT NULL COMMENT '系统编号',
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `tsys_dict`;
 CREATE TABLE `tsys_dict` (
   `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '编号（自增长）',
