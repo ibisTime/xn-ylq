308768bc03fffb36ad2c4a9ba30ca0606345865e
haiqingzheng <leo.zheng@hichengdai.com>
2017-10-11 14:45:37 +0800

    v122 bug fix

--- src/main/java/com/cdkj/ylq/ao/impl/AccountAOImpl.java
+++ src/main/java/com/cdkj/ylq/ao/impl/AccountAOImpl.java
@@ -8,6 +8,9 @@
  */
 package com.cdkj.ylq.ao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +18,7 @@ import com.cdkj.ylq.ao.IAccountAO;
 import com.cdkj.ylq.bo.IAccountBO;
 import com.cdkj.ylq.bo.IBorrowBO;
 import com.cdkj.ylq.domain.Borrow;
+import com.cdkj.ylq.enums.EBorrowStatus;
 import com.cdkj.ylq.exception.BizException;
 
 /** 
@@ -38,13 +42,17 @@ public class AccountAOImpl implements IAccountAO {
     public void editBankcard(String userId, String code, String realName,
             String bankcardNumber, String bankCode, String bankName,
             String status) {
-        Borrow borrow = borrowBO.getCurrentBorrow(userId);
-        if (borrow == null) {
-            accountBO.editBankcard(code, realName, bankcardNumber, bankCode,
-                bankName, status);
-        } else {
+        Borrow condition = new Borrow();
+        List<String> statusList = new ArrayList<String>();
+        statusList.add(EBorrowStatus.LOANING.getCode());
+        statusList.add(EBorrowStatus.OVERDUE.getCode());
+        condition.setApplyUser(userId);
+        condition.setStatusList(statusList);
+        List<Borrow> borrows = borrowBO.queryBorrowList(condition);
+        if (borrows.size() > 0) {
             throw new BizException("xn623850", "您当前有正在进行的借款，暂时无法修改银行卡！");
         }
-
+        accountBO.editBankcard(code, realName, bankcardNumber, bankCode,
+            bankName, status);
     }
 }
