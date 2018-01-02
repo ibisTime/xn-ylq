fe4be5a9109d9902fe07cc735ce708e352cf176c
chenshan <chenshan@localhost>
2018-01-02 11:11:34 +0800

    脚本更新

--- src/main/java/com/cdkj/ylq/common/SysConstants.java
+++ src/main/java/com/cdkj/ylq/common/SysConstants.java
@@ -16,7 +16,7 @@ public class SysConstants {
 
     public static final String AMOUNT_VALID_DAYS = "amountValidDays"; // 额度有效天数
 
-    public static final String SEND_SMS_COUNT = "sendSmsCount"; // 催收联系人数量
+    // public static final String SEND_SMS_COUNT = "sendSmsCount"; // 催收联系人数量
 
     public static final String RENEWAL_STEP = "renewalStep"; // 续期步长
 
