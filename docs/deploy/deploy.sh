0ef4bbe7d01ca5f218c3a90fe7c6eaeec66b0a63
haiqingzheng <leo.zheng@hichengdai.com>
2018-11-24 16:12:30 +0800

    部署脚本修改成最新版

--- docs/deploy/deploy.sh
+++ docs/deploy/deploy.sh
@@ -6,43 +6,47 @@ set war_name "xn-ylq"
 set host_ip "120.26.6.213"
 set host_pwd "cdkj123456Q"
 
-spawn ssh root@${host_ip}
-expect "*password:"
-send "${host_pwd}\r"
-#关闭tomcat
-expect "*#"
-send "${tomcat_path}/bin/shutdown.sh\r"
-
-expect "*#"
-send "ps -efww|grep -w ${tomcat_path}|grep -v grep|cut -c 9-15|xargs kill -9\r"
-
-expect "*#"
-send "rm -rf ${tomcat_path}/webapps/${war_name}\r"
-
-expect "*#"
-send "rm -rf ${tomcat_path}/webapps/c${war_name}.war\r"
-
-expect "*#"
-send "exit\r"
-
-#2.传输文件
-spawn scp ./${war_name}.war root@${host_ip}:${tomcat_path}/webapps
+######## 1.传输文件至服务器根目录 ########
+spawn scp ./${war_name}.war root@${host_ip}:~/
 expect "*password:"
 send "${host_pwd}\r"
 set timeout 300
 send "exit\r"
 expect eof
 
-#3.重启tomcat
+######## 2.登录服务器 ########
 spawn ssh root@${host_ip}
 expect "*password:"
 send "${host_pwd}\r"
+
+######## 4.杀掉这个tomcat相关进程 ########
+expect "*#"
+send "ps -efww|grep -w ${tomcat_path}|grep -v grep|cut -c 9-15|xargs kill -9\r"
+
+######## 5.删除原来的部署包 ########
+expect "*#"
+send "rm -rf ${tomcat_path}/webapps/${war_name}\r"
+
+expect "*#"
+send "rm -rf ${tomcat_path}/webapps/${war_name}.war\r"
+
+######## 5.移动新包至tomcat ########
+expect "*#"
+send "mv ~/${war_name}.war ${tomcat_path}/webapps/\r"
+
+######## 6.重启tomcat ########
 expect "*#"
 send "${tomcat_path}/bin/startup.sh\r"
 
 expect "*#"
 send "exit\r"
-#interact
+
+
+
+
+
+
+
 
 
 
